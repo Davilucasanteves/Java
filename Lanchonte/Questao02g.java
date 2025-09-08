46e77d0de904d07ff26d9f993df92f834846345c
Davilucasanteves <24680000za@gmail.com>
2025-09-07 23:03:14 -0300

    tratando Lanchonete

--- Lanchonte/Questao02g.java
+++ Lanchonte/Questao02g.java
@@ -7,7 +7,7 @@ public class Questao02g
                 + "2 - Fazer pedido\n"
                 + "3 - Inserir itens\n"
                 + "4 - Calcular valor do pedido\n"
-                + "5 - Comprar produto\n" //comprar de produtos para lanchonete(COMPRAR)
+                + "5 - Comprar produto\n" //comprar de produtos para lanchonete
                 + "0 - sair";
     }
     public static void main(String[] args) {
@@ -41,22 +41,22 @@ public class Questao02g
                 case 2: 
                     System.out.println("Digite o código do produto a vender: ");
                     int codigo = teclado.nextInt();
-                    while(codigo>p.getNumProdutos()){
-                        System.out.println("Código inválido! Digite de novo\nDigite o código do produto a vender: ");
-                        codigo = teclado.nextInt();
+                    if(codigo>contProduto){
+                        System.out.println("Código inválido! Digite um válido ou cadastre algum produto!\n");
+                        break;
                     }
                     if(item==null){
                         System.out.println("Insira itens primeiro!\n");
                     }
                     produtos[codigo].venderProduto(item.getQuantidade());
-                    System.out.println("\nAgora tem "+produtos[codigo].getEstoque()+" desse produto.");
+                    System.out.println("Agora tem "+produtos[codigo].getEstoque()+" desse produto.\n");
                     break;
                 case 3:
                     System.out.println("Digite o código do produto a inserir na compra: ");
                     codigo = teclado.nextInt();
-                    while(codigo>p.getNumProdutos()){
-                        System.out.println("Código inválido! Digite de novo\nDigite o código do produto a vender: ");
-                        codigo = teclado.nextInt();
+                    if(codigo>contProduto){
+                        System.out.println("Código inválido! Digite um válido ou cadastre algum produto!\n");
+                        break;
                     }
                     System.out.println("Digite a quantidade do produto a inserir na compra: ");
                     int qtd = teclado.nextInt();
@@ -69,18 +69,34 @@ public class Questao02g
                     if (pedido!= null) {
                         System.out.println("Seu pedido deu: " + pedido.calcularTotalPedido()+"\n");
                     } else {
-                        System.out.println("Nenhum pedido criado!");
+                        System.out.println("Nenhum pedido criado!\n");
                     }
                     break;
                 case 5:
                     System.out.println("Digite o nome do produto: ");
                     nome = teclado.next();
-                    System.out.println("Digite o valor do produto: ");
-                    valor = teclado.nextDouble();
-                    System.out.println("Digite a quantidade do produto a ser comprado: ");
-                    quantidade = teclado.nextInt();
-                    Produto p2 =  new Produto(nome, valor);//não pode ter quantidade para não sobrescrever os trem tudo
-                    p2.comprarProduto(quantidade);
+                    boolean achou= false;
+
+                    if(pedido != null){
+                        for(Item i : pedido.getItens()){
+                            if(i != null){
+                                Produto produto = i.getProduto();
+                                if(produto != null && produto.getNome().equalsIgnoreCase(nome)){
+                                    achou = true;
+                                    System.out.println("Digite o valor do produto: ");
+                                    valor = teclado.nextDouble();
+                                    System.out.println("Digite a quantidade do produto a ser comprado: ");
+                                    quantidade = teclado.nextInt();
+                                    Produto p2 =  new Produto(nome, valor);//não pode ter quantidade, esse caso é só compra, não cadastro
+                                    p2.comprarProduto(quantidade);
+                                }
+                            }
+                        }
+                    }
+
+                    if(!achou){
+                        System.out.println("Produto não consta nos cadastros, cadastre-o!\n");
+                    }
                     break;
                 default:
                     System.err.println("Opção inválida!");
